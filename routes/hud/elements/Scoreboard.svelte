30256a503ca3c074588c7fe1a599132962b32141
lucas <143325613+larryngton@users.noreply.github.com>
2024-06-03 17:54:51 +0300

    Add files via upload

---
+++ routes/hud/elements/Scoreboard.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+    import {listen} from "../../../integration/ws";
+    import type {PlayerData, Scoreboard} from "../../../integration/types";
+    import TextComponent from "../../menu/common/TextComponent.svelte";
+    import type {ClientPlayerDataEvent} from "../../../integration/events";
+
+    let scoreboard: Scoreboard | null = null;
+
+    listen("clientPlayerData", (e: ClientPlayerDataEvent) => {
+        const playerData: PlayerData = e.playerData;
+        scoreboard = playerData.scoreboard;
+    });
+</script>
+
+{#if scoreboard}
+    <div class="scoreboard">
+        {#if scoreboard.header}
+            <div class="header">
+                <TextComponent fontSize={14} allowPreformatting={true} textComponent={scoreboard.header}/>
+            </div>
+        {/if}
+        <div class="entries">
+            {#each scoreboard.entries as {name, score}}
+                <div class="row">
+                    <TextComponent fontSize={14} allowPreformatting={true} textComponent={name}/>
+                    <TextComponent fontSize={14} allowPreformatting={true} textComponent={score}/>
+                </div>
+            {/each}
+        </div>
+    </div>
+{/if}
+
+<style lang="scss">
+  @import "../../../colors.scss";
+
+  .scoreboard {
+    width: max-content;
+    //position: fixed;
+    //left: 15px;
+    //top: 550px;
+    border-radius: 12px;
+    overflow: hidden;
+    font-size: 14px;
+    font-family: sans-serif;
+    box-shadow: 0px 0px 4px 4px rgba($accent-color, $transparency);
+  }
+
+  .entries {
+    background-color: rgba($scoreboard-base-color, $transparency);
+    padding: 10px;
+    font-family: sans-serif;
+  }
+
+  .row {
+    display: flex;
+    column-gap: 15px;
+    justify-content: space-between;
+    font-family: sans-serif;
+  }
+
+  .header {
+    text-align: center;
+    background-color: rgba($scoreboard-base-color, $transparency);
+    padding: 7px 10px;
+    font-weight: 600;
+    font-family: sans-serif;
+  }
+</style>
---
+++ routes/hud/elements/notifications/Notification.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+    export let title: string;
+    export let message: string;
+    export let severity: string;
+</script>
+
+<div class="notification">
+    <div class="icon {severity.toString().toLowerCase()}"/>
+    <div class="title">{title}</div>
+    <div class="message">{message}</div>
+</div>
+
+<style lang="scss">
+  @import "../../../../colors.scss";
+  
+  .notification {
+    min-width: 170px;
+    display: grid;
+    grid-template-areas:
+            "a b"
+            "a c";
+    grid-template-columns: max-content 1fr;
+    column-gap: 10px;
+    background: rgba(0, 0, 0, $transparency);
+    border-radius: 12px;
+    overflow: hidden;
+    padding-right: 12px;
+    padding-top: 6px;
+    padding-bottom: 6px;
+    padding-left: 6px;
+    margin-bottom: 10px;
+    box-shadow: 0px 0px 4px 4px rgba($accent-color, $transparency);
+  }
+
+  .icon {
+    height: 40px;
+    width: 40px;
+    background-position: center;
+    background-repeat: no-repeat;
+    border-radius: 6px;
+    grid-area: a;
+    transition: background-color 0.2s;
+    position: relative;
+
+    &.success {
+      background-color: #3b3b3b;
+      background-image: url("/img/hud/notification/icon-success.svg");
+    }
+
+    &.error {
+      background-color: #3b3b3b;
+      background-image: url("/img/hud/notification/icon-error.svg");
+    }
+
+    &.info {
+      background-color: #3b3b3b;
+      background-image: url("/img/hud/notification/icon-info.svg");
+    }
+
+    &.disabled,
+    &.enabled {
+      &::after {
+        content: "";
+        position: absolute;
+        height: 10px;
+        width: 10px;
+        border-radius: 6px;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        background: white;
+        transition: all 0.2s ease;
+      }
+
+      background-image: url("/img/hud/notification/icon-toggle.svg");
+    }
+
+    &.enabled {
+      background-color: #3b3b3b;
+
+      &::after {
+        left: 62%;
+      }
+    }
+
+    &.disabled {
+      background-color: #3b3b3b;
+
+      &::after {
+        left: 38%;
+      }
+    }
+  }
+
+  .title {
+    grid-area: b;
+    font-size: 14px;
+    color: white;
+    font-weight: 600;
+  }
+
+  .message {
+    grid-area: c;
+    font-size: 12px;
+    color: #cbd1e3;
+  }
+</style>
